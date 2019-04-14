146d229779d8eec53692e72e0a3278a4ec5d5df2
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-04-14 01:50:12 -0400

    Setting up database

---
+++ data/migrations/20190413231813_Prisons.js
@@ -0,0 +1,50 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('prisons', col => {
+        col.increments();
+        col.string('username').notNullable().unique();
+        col.string('address').notNullable();
+        col.string('name').notNullable();
+    })
+    .createTable('prisoners', col => {
+        col.increments();
+        col.string('name').notNullable();
+        col.integer('prison_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('Prisons')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE');
+        col.boolean('canHaveWorkLeave').defaultTo(false);
+    })
+    .createTable('skills', col => {
+        col.increments();
+        col.string('name').notNullable();
+    })
+    .createTable('prisoners_skills', col => {
+        col.increments();
+        col.integer('prisoners_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('prisoners')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+        col.integer('skills_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('skills')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+    })
+}
+
+exports.down = function(knex, Promise) {
+    return knex.schema
+        .dropTableIfExists('prisons')
+        .dropTableIfExists('prisoners')
+        .dropTableIfExists('skills')
+        .dropTableIfExists('prisoners_skills')
+};
