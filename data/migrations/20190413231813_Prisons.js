11ff9e079d22b1c6c0e0fab208b6508e0c23ad64
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-04-15 03:04:55 -0400

    Fixed typo

--- data/migrations/20190413231813_Prisons.js
+++ data/migrations/20190413231813_Prisons.js
@@ -14,7 +14,7 @@ exports.up = function(knex, Promise) {
             .unsigned()
             .notNullable()
             .references('id')
-            .inTable('Prisons')
+            .inTable('prisons')
             .onDelete('RESTRICT')
             .onUpdate('CASCADE');
         col.boolean('canHaveWorkLeave').defaultTo(false);
